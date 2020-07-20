09bf4a382025a68f6c6ddde18f39bcb913a0a332
JZPoteat <jzpoteat@gmail.com>
2020-07-20 14:59:23 -0400

    solved problems 5 and 6

--- hash-drills.js
+++ hash-drills.js
@@ -1,69 +1,69 @@
 const { HashMap } = require('./hash-map');
-const { HashMap_SepChain } = require('./hash-sep-chain')
+const { HashMap_SepChain } = require('./hash-sep-chain');
 
 function main() {
-     const lotr = new HashMap();
-     lotr.MAX_LOAD_RATIO = 0.5;
-     lotr.SIZE_RATIO = 3;
+    const lotr = new HashMap();
+    lotr.MAX_LOAD_RATIO = 0.5;
+    lotr.SIZE_RATIO = 3;
 
-     lotr.set('Hobbit', 'Bilbo');
-     lotr.set('Hobbit', 'Frodo');
-     lotr.set('Wizard', 'Gandalf');
-     lotr.set('Human', 'Aragorn');
-     lotr.set('Elf', 'Legolas');
-     lotr.set('Maiar', 'The Necromancer');
-     lotr.set('Maiar', 'Sauron');
-     lotr.set('RingBearer', 'Gollum');
-     lotr.set('LadyOfLight', 'Galadriel');
-     lotr.set('HalfElven', 'Arwen');
-     lotr.set('Ent', 'Treebeard');
+    lotr.set('Hobbit', 'Bilbo');
+    lotr.set('Hobbit', 'Frodo');
+    lotr.set('Wizard', 'Gandalf');
+    lotr.set('Human', 'Aragorn');
+    lotr.set('Elf', 'Legolas');
+    lotr.set('Maiar', 'The Necromancer');
+    lotr.set('Maiar', 'Sauron');
+    lotr.set('RingBearer', 'Gollum');
+    lotr.set('LadyOfLight', 'Galadriel');
+    lotr.set('HalfElven', 'Arwen');
+    lotr.set('Ent', 'Treebeard');
 
-     //Print your hash map and notice the length and items that are hashed 
-     //in your hash map. Have you hashed all the items you were asked to?
-     console.log(lotr);
-     //Length = 9
-     //Some items are missing due to duplicate keys.
+    //Print your hash map and notice the length and items that are hashed 
+    //in your hash map. Have you hashed all the items you were asked to?
+    console.log(lotr);
+    //Length = 9
+    //Some items are missing due to duplicate keys.
 
-     //Retrieve the value that is hashed in the key "Maiar" and Hobbit.
-     console.log('Maiar:', lotr.get('Maiar'));
-     console.log('Hobbit:', lotr.get('Hobbit'));
+    //Retrieve the value that is hashed in the key "Maiar" and Hobbit.
+    console.log('Maiar:', lotr.get('Maiar'));
+    console.log('Hobbit:', lotr.get('Hobbit'));
 
-     //What are the values of Maiar and Hobbit that you have? 
-     //Sauron and Frodo.
+    //What are the values of Maiar and Hobbit that you have? 
+    //Sauron and Frodo.
 
-     //Is there a discrepancy? Explain your answer.
-     //We only show one of the two values for each key because 
-     //we don't have anything to handle collisions in place
+    //Is there a discrepancy? Explain your answer.
+    //We only show one of the two values for each key because 
+    //we don't have anything to handle collisions in place
 
-     //What is the capacity of your hash table after you have hashed all 
-     //the above items? Explain your answer.
+    //What is the capacity of your hash table after you have hashed all 
+    //the above items? Explain your answer.
 
-     console.log(lotr._capacity);
-     //Should be 24. Console.log returns 8.
-     //Initial capacity is 8, and once 50% of that was passed, we multiplied 
-     //it by our size ratio of 3.
+    console.log(lotr._capacity);
+    //Should be 24. Console.log returns 8.
+    //Initial capacity is 8, and once 50% of that was passed, we multiplied 
+    //it by our size ratio of 3.
 }
 
 main();
 
 //2. WhatDoesThisDo
 const WhatDoesThisDo = function () {
-     let str1 = 'Hello World.';
-     let str2 = 'Hello World.';
-     let map1 = new HashMap();
-     map1.set(str1, 10);
-     map1.set(str2, 20);
-     let map2 = new HashMap();
-     let str3 = str1;
-     let str4 = str2;
-     map2.set(str3, 20);
-     map2.set(str4, 10);
+    let str1 = 'Hello World.';
+    let str2 = 'Hello World.';
+    let map1 = new HashMap();
+    map1.set(str1, 10);
+    map1.set(str2, 20);
+    let map2 = new HashMap();
+    let str3 = str1;
+    let str4 = str2;
+    map2.set(str3, 20);
+    map2.set(str4, 10);
 
-     console.log(map1.get(str1)); //20
-     console.log(map2.get(str3)); //10 
-     console.log(map1.get(str2)); //20
-     console.log(map2.get(str2)); //10
-}
+    console.log(map1.get(str1)); //20
+    console.log(map2.get(str3)); //10 
+    console.log(map1.get(str2)); //20
+    console.log(map2.get(str2)); //10
+};
 
 WhatDoesThisDo();
 
@@ -106,17 +106,17 @@ threeTwo.set(10);
 //“google”, the result after deletion is “gole”. Test your program with a sentence 
 //as well such as "google all that you think can think of".
 function removeDupe(string) {
-     const hashDupe = new Map();
-     let newString = '';
+    const hashDupe = new Map();
+    let newString = '';
 
-     string.split('').forEach(letter => {
-          if (!hashDupe.has(letter)) {
-               hashDupe.set(letter, '');
-               newString += letter;
-          }
-     });
+    string.split('').forEach(letter => {
+        if (!hashDupe.has(letter)) {
+            hashDupe.set(letter, '');
+            newString += letter;
+        }
+    });
 
-     console.log(newString)
+    console.log(newString);
 }
 
 removeDupe('google'); //gole
@@ -130,10 +130,56 @@ removeDupe('google all that you think can think of'); //gole athyuinkcf
 //false, because there's no anagram for "north" that would be a palindrome.
 
 function palindrome(str) { 
-     let reverseStr = str.toLowerCase().match(/[a-z]/gi).reverse(); 
-     return reverseStr.join('') === reverseStr.reverse().join(''); 
+//in order for string to be palindrome, must have either 1 or an even number of occurences for each character
+    let strObj = {};
+    let strsplit = str.split('');
+    strsplit.forEach(char => {
+        if(char in strObj) {
+            strObj[char] = strObj[char] + 1;
+        } else {
+            strObj[char] = 1;
+        }
+    });
+    let count = 0;
+    for(const [key, value] of Object.entries(strObj)) {
+        if(value === 1) {
+            count++;
+        }  
+        if (value % 2 === 1 && value !== 1) {
+            return false;
+        }
+    }
+    if(count > 1 ) {
+        return false;
+    }
+    return true;
+}
+
+// console.log(palindrome('acecarr'));
+// console.log(palindrome('aaatyaaa'));
+
+function anagramGrouping(arr) {
+    //takes an array of strings, and groups the array by anagram
+    let anagramMap = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        let splitArr = arr[i].split('').sort().join('');
+        if(!anagramMap.has(splitArr)) {
+            anagramMap.set(splitArr, [arr[i]]);
+        } else {
+            anagramMap.set(anagramMap.get(splitArr), anagramMap.get(splitArr).push(arr[i]));
+        }
+    }
+    let myAns = [];
+    Array.from(anagramMap.values()).forEach(e => {
+        let eType = typeof(e);
+        if(eType === 'object') {
+            myAns.push(e);
+        }
+    });
+    return myAns;
 }
 
 
-console.log(palindrome('acecarr'))
-console.log(palindrome('north'))
+console.log(anagramGrouping(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
+
+
